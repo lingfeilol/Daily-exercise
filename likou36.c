6e39fead93d543dba7cc67a4cf52b183b4f17fbe
lingfeilol <lingfeilol@163.com>
2019-12-13 23:56:18 +0800

    26. 删除排序数组中的重复项

---
+++ likou36.c
@@ -0,0 +1,12 @@
+int removeDuplicates(int* nums, int numsSize){
+    if(numsSize==0)
+        return 0;
+    int i=0;
+    int k=0;
+   for(i=1;i<numsSize;i++)
+    {
+        if(nums[i]!=nums[i-1])
+            nums[k++]=nums[i];
+    }
+    return  k;
+}
